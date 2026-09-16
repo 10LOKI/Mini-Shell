a40e8d0a651da6028da32a3f9746568635083771
10k1 <ayoubouharda33@gmail.com>
2026-09-16 23:01:36 +0100

    Construct of LogAnalyzerService

--- src/main/java/ma/youcode/lineperm/service/LogAnalyzerService.java
+++ src/main/java/ma/youcode/lineperm/service/LogAnalyzerService.java
@@ -1,9 +1,24 @@
 package ma.youcode.lineperm.service;
 
 import ma.youcode.lineperm.model.AccesLog;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 public class        LogAnalyzerService
 {
+    private list<AccesLog>  logs;
+
+    public LogAnalyzerService(String cheminFichier)
+    {
+        this.logs = new ArrayList<>();
+        chargerLogs(cheminFichier);
+    }
+
+    
+
     public long                 nbrActions()
     {
 
