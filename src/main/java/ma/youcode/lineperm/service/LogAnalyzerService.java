90732beb1da32a69f6eaf565efd8cebb3a4b1faf
10k1 <ayoubouharda33@gmail.com>
2026-09-17 09:18:57 +0100

    la methode utilisateur Action

--- src/main/java/ma/youcode/lineperm/service/LogAnalyzerService.java
+++ src/main/java/ma/youcode/lineperm/service/LogAnalyzerService.java
@@ -56,7 +56,7 @@ public class        LogAnalyzerService
     
     public Map<String, Long>    utilisateurAction()
     {
-
+        return (logs.stream().map(AccesLog::getUtilisateur).distinct().collect(Collectors.toList()));
     }
     public List<Map.Entry<String, Long>>    topFichiers()
     {
