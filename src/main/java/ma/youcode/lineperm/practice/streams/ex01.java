0d9e17b366ff42c568f8329b0d755713d592a9c4
10k1 <ayoubouharda33@gmail.com>
2026-09-24 14:22:37 +0000

    une pause

--- src/main/java/ma/youcode/lineperm/practice/streams/ex01.java
+++ src/main/java/ma/youcode/lineperm/practice/streams/ex01.java
@@ -1,6 +1,6 @@
 package ma.youcode.lineperm.practice.streams;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 public class ex01
 {
@@ -8,7 +8,8 @@ public class ex01
     {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
-        List<Integer> result = numbers.stream().map(n -> n * 2).collect(Collectors.toList());
-        System.out.println(result);
+        // List<Integer> result = numbers.stream().map(n -> n * 2).collect(Collectors.toList());
+        // System.out.println(result);
+        List<Integer> loop = new ArrayList<>();
     }
 }
