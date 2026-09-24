fce5cf75f8a992885338a54dd01213b026c631ab
10k1 <ayoubouharda33@gmail.com>
2026-09-24 14:44:23 +0000

    la creation du UserDao et l'appel des methodes pour l'override

--- src/main/java/ma/youcode/lineperm/dao/UserDao.java
+++ src/main/java/ma/youcode/lineperm/dao/UserDao.java
@@ -1,5 +1,24 @@
 package ma.youcode.lineperm.dao;
 
-public class UserDao {
-    
+import ma.youcode.lineperm.model.User;
+import java.sql.*;
+public class UserDao extends AbstractDao<User>
+{
+    @Override
+    public void     delete(int id)
+    {
+
+    }
+
+    @Override
+    public User    findById(int id)
+    {
+        return (User);
+    }
+
+    @Override
+    public void     save(User t)
+    {
+
+    }
 }
