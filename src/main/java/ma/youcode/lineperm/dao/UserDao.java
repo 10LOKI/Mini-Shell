6f9a00f4dcb2eb65f472d4cd9db784edd182ff47
10k1 <ayoubouharda33@gmail.com>
2026-09-25 14:29:02 +0000

    override delete and findById methods even if i don't wanna use them

--- src/main/java/ma/youcode/lineperm/dao/UserDao.java
+++ src/main/java/ma/youcode/lineperm/dao/UserDao.java
@@ -4,6 +4,15 @@ import java.sql.*;
 import ma.youcode.lineperm.model.User;
 public class UserDao extends AbstractDao<User>
 {
+    @Override
+    public void delete(int id) {
+        throw new UnsupportedOperationException("Delete operation is not supported by UserDao.");
+    }
+
+    @Override
+    public User findById(int id) {
+        throw new UnsupportedOperationException("FindById operation is not supported by UserDao.");
+    }
     public User findByLogin(String login) 
     {
         String sqlQuery = "select * from users where login = ?";
@@ -26,36 +35,7 @@ public class UserDao extends AbstractDao<User>
         }
         return null;
     }
-    
-    @Override
-    public void     delete(int id)
-    {
-        String sqlQuery = "delete * from users where id = ?";
-    }
 
-    @Override
-    public User    findById(int id)
-    {
-        String sqlQuery = "select * from users where id = ?";
-        try (PreparedStatement stmt = getConnection().prepareStatement(sqlQuery))
-        {
-            stmt.setInt(1, id);
-            try (ResultSet result = stmt.executeQuery())
-            {
-                if (result.next())
-                {
-                    User user = new User(result.getInt("id") ,result.getString("login"), result.getString("password"));
-                    // System.out.println("he's found");
-                    return (user);
-                }
-            }
-        }
-        catch (SQLException e)
-        {
-            throw new RuntimeException("Database error in findById : " + e.getMessage(), e);
-        }
-        return null;
-    }
 
     @Override
     public void     save(User user)
