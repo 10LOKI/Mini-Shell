22c5a5ab211c10c212ddf1983c7531faaa714260
10k1 <ayoubouharda33@gmail.com>
2026-09-25 10:58:26 +0000

    j'ai ajoute la methode getId and id attribut dans le model user

--- src/main/java/ma/youcode/lineperm/model/User.java
+++ src/main/java/ma/youcode/lineperm/model/User.java
@@ -2,15 +2,27 @@ package ma.youcode.lineperm.model;
 
 public class User
 {
-    private String login;
-    private String passwordHash;
+    private final int       id;
+    private final String    login;
+    private final String    passwordHash;
 
-    public User(String login, String passwordHash)
+    public User(String login, String passwordHash) 
     {
+        this(0, login, passwordHash);
+    }
+
+    public User(int id, String login, String passwordHash) 
+    {
+        this.id = id;
         this.login = login;
         this.passwordHash = passwordHash;
     }
 
+    public int      getId()
+    {
+        return (id);
+    }
+    
     public String getLogin()
     {
         return (login);
